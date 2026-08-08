3f60d02ae5d6edf4ee14e2c7b9c286abd192dd00
ibrahim milanho <ibrahimmilanho6803@gmail.com>
2026-08-08 15:12:44 +0100

    Correction lien dashboard

--- paiement.py
+++ paiement.py
@@ -91,7 +91,7 @@ PAGE_PAIEMENT = """
         <p style="color:#aaa;">Conservez cette cle precieusement</p>
         <p style="color:#aaa;">Vérifiez vos SPAM si vous ne trouvez pas le message</p>
     </div>
-    <a href="https://triple-elite-vip-dashboard.onrender.com/login" class="btn">Se connecter au Dashboard</a>
+    <a href="https://triple-elite-vip-dashboard.onrender.com/login" class="btn">Se connecter</a>
 </div>
         </div>
         
