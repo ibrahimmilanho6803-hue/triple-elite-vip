ea9f6dd70c576a6c75495470f1c7d79eca814037
ibrahim milanho <ibrahimmilanho6803@gmail.com>
2026-08-11 16:15:13 +0100

    Ajout lien PayPal annuel

--- paiement.py
+++ paiement.py
@@ -12,7 +12,7 @@ lm = LicenseManager()
 
 PAYPAL_LINK_MENSUEL = "https://www.paypal.com/ncp/payment/VQLWDAY9P9RYQ"
 # Ajoutez un autre lien pour l'annuel quand vous l'aurez créé
-PAYPAL_LINK_ANNUEL = "https://www.paypal.com/ncp/payment/VOTRE_AUTRE_LIEN"
+PAYPAL_LINK_ANNUEL = "https://www.paypal.com/ncp/payment/DXQ4TNVK8JW9S
 
 PAGE_PAIEMENT = """
 <!DOCTYPE html>
