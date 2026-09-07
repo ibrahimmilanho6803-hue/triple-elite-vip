8c46083aafd9b55cfed7e8b753330caff63116c4
ibrahimmilanho6803-hue <ibrahimmilanho6803@gmail.com>
2026-09-07 17:09:05 +0100

    Change Anthropics client API key
    Updated API key for the Anthropics client.

--- analyzer.py
+++ analyzer.py
@@ -8,7 +8,7 @@ class MatchAnalyzer:
         self.db = 'triple_elite.db'
         self.conn = sqlite3.connect(self.db)
         self.cursor = self.conn.cursor()
-        self.client = anthropic.Anthropic(api_key="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")
+        self.client = anthropic.Anthropic(api_key="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")
 
     def get_team_stats(self, team_name):
         self.cursor.execute("SELECT * FROM team_stats WHERE team_name = ?", (team_name,))
